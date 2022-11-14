2c728a5231f777d25d14d9dd69853a5ee7b91883
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2022-11-14 23:31:22 +0700

    Выполнил задание 41.

---
+++ Task41/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+
+*/
+
+Console.Write("Введите числа через запятую - ");
+
+string[] stringArr = Console.ReadLine().Split(',');
+
+int[] intArr = new int [stringArr.Length];
+
+int nonZero = 0;
+
+for (int i = 0; i < stringArr.Length; i++) {
+    intArr[i] = int.Parse(stringArr[i]);
+    if (intArr[i] > 0) nonZero += 1;
+}
+
+Console.WriteLine($"В введеной последовательности положительных чисел -> {nonZero}.");
